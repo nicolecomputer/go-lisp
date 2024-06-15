a5a7a6512782961624be8124455beba3b4e70fd8
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2024-06-15 16:17:26 -0600

    pairing together

--- evaluate/evaluate.go
+++ evaluate/evaluate.go
@@ -1,6 +1,7 @@
 package lisp
 
 import (
+	lisp "lisp/parse"
 	parser "lisp/parse"
 )
 
@@ -11,6 +12,42 @@ type (
 
 func evaluate(exp parser.Expression, env Environment) (error, Result, Environment) {
 	return nil, Result{}, Environment{}
+}
+
+func evaluateList(list lisp.List) (error, lisp.Expression) {
+	operator := list.Children[0]
+
+	switch operatorAtom := operator.(type) {
+	case lisp.OperatorAtom:
+		if operatorAtom.Value == "+" {
+			thingsToAdd := list.Children[1:]
+			var total float64 = 0
+			for _, child := range thingsToAdd {
+				switch childValue := child.(type) {
+				case lisp.NumberAtom:
+					total += childValue.Value
+				}
+			}
+
+			return nil, lisp.NumberAtom{total}
+		}
+	}
+	return nil, nil
+}
+
+func evaluateStr(expression string) (error, lisp.Expression) {
+	error, value := lisp.Parse(expression)
+
+	if error != nil {
+		return error, nil
+	}
+
+	switch value := value.(type) {
+	case lisp.List:
+		return evaluateList(value)
+	default:
+		return nil, value
+	}
 }
 
 // It would be nice to have
